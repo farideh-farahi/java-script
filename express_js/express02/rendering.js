9e00edf6cbea4ba3aa693242479fdb47d96868ea
farideh.farahi <faridehfarahi1379f.x@gmail.com>
2025-03-11 16:22:08 +0330

    git ignore

--- express_js/express02/rendering.js
+++ express_js/express02/rendering.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const helmet = require('helmet');
+const helmet = require('./.gitignore/node_modules/helmet/index.d.cts');
 app.use(helmet());
 app.use(
     helmet({
