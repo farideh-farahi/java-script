ed9b79f93d10fc15fb352ee06504fe4ed977b120
farideh.farahi <faridehfarahi1379f.x@gmail.com>
2025-03-22 14:11:18 +0330

    login & logoutpage

---
+++ express_js/express03/loginSite.js
@@ -0,0 +1,63 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const cookieParser = require('cookie-parser');
+
+const helmet = require('helmet');
+
+app.use(helmet());
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                "script-src": ["'self'", "'unsafe-inline'"],
+            },
+        },
+    })
+);
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cookieParser());
+
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname, 'views'));
+
+app.get('/',(req, res, next)=>{
+    res.send('Sanity check')
+});
+
+app.get('/login',(req, res, next) => {
+    res.render('login')
+});
+
+app.post('/process_login',(req, res, next) => {
+    const username = req.body.username
+    const password = req.body.password
+    if (password === "x"){
+        res.cookie('username', username)
+        res.redirect('/welcome')
+    }else{
+        res.redirect('login?msg=fail')
+    }
+});
+
+app.get('/welcome',(req, res, next) => {
+    res.render('welcome',{
+        username : req.cookies.username
+    })
+});
+
+app.get('/logout',(req, res, next) => {
+    res.clearCookie('username')
+    res.redirect('/login')
+});
+
+app.listen(3000,() => {
+    console.log("Server is running on http://localhost:3000");
+});
+
+
