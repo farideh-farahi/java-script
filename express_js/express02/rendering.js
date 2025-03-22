579114630fe382a1c750956e9bd5d2b373a54a2f
farideh.farahi <faridehfarahi1379f.x@gmail.com>
2025-03-22 11:32:15 +0330

    rendering

--- express_js/express02/rendering.js
+++ express_js/express02/rendering.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const helmet = require('./.gitignore/node_modules/helmet/index.d.cts');
+/*const helmet = require('helmet');
 app.use(helmet());
 app.use(
     helmet({
@@ -14,7 +14,7 @@ app.use(
         },
     })
 );
-
+*/
 
 
 app.use(express.static('public'));
@@ -25,10 +25,24 @@ app.use(express.urlencoded());
 app.set('view engine' , 'ejs');
 //app.set('view engine' , 'hbs');
 //app.set('view engine' , 'pug');
-app.set('view ' , path.join(__dirname, 'views'));
+app.set('views' , path.join(__dirname, 'views'));
+
+function validateUser(req, res, next){
+    res.locals.validate = true
+    next()
+}
+app.use(validateUser)
 
 app.get('/',(req, res, next) =>{
-    res.render('index');
+    res.render('index',/*{
+        async: true,
+        msg : "Succcess!",
+        msg2 : "Failure",
+        html : "<p><p/>"
+    }*/);
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
+
